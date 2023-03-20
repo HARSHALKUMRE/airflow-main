15c51348962f4d543eb02a7f837e9ff8f4d1c941
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-03-20 23:28:30 +0530

    code updated

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -4,9 +4,13 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import pandas as pd
 
-def first_fuction_execute():
-    print("hello world")
-    return "hello world"
+def first_fuction_execute(**context):
+    print("first function execute")
+    context['ti'].xcom_push(key='mykey',value='first_function_execute say hello')
+    
+def second_function_execute(**context):
+    instance = context.get("ti").xcom_pull(key='mykey')
+    print("i am in second function execute got value : {} from function 1".format(instance))
 
 with DAG(
     dag_id = 'first_dag',
@@ -20,8 +24,15 @@ with DAG(
     catchup=False
 ) as f:
     first_function_execute = PythonOperator(
-        task_id = 'first_function_execute',
-        python_callable = first_function_execute,
+        task_id = 'first_fuction_execute',
+        python_callable = first_fuction_execute,
+        op_kwargs = {"name":"Harshal"}
+    )
+    second_function_execute = PythonOperator(
+        task_id = 'second_function_execute',
+        python_callable = second_function_execute,
+        provide_context = True
     )
     
+first_function_execute>>second_function_execute
     
