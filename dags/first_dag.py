4409e00f26a13ba77f7e9298fbe54004175a7da0
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-03-20 19:31:28 +0530

    code updated

---
+++ dags/first_dag.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+import pandas as pd
+
+def first_fuction_execute():
+    print("hello world")
+    return "hello world"
+
+with DAG(
+    dag_id = 'first_dag',
+    schedule_interval = '@daily',
+    default_args = {
+        "owner":"airflow",
+        "retries":1,
+        "retry_delay":timedelta(minutes=5),
+        "start_date":datetime(2023,3,20),
+    },
+    catchup=False
+) as f:
+    first_function_execute = PythonOperator(
+        task_id = 'first_function_execute',
+        python_callable = first_function_execute,
+    )
+    
+    
